0592b6a9cc835d8e414ba4a5be8c2b5c819a2599
Lampros Papadimitriou <papadimitriou@tyntec.com>
2020-03-26 01:14:28 +0100

    Added first pie chart

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,74 +1,63 @@
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import PieData from '@/types/PieData';
 import * as d3 from 'd3';
-
-interface PieData {
-  name: string;
-  value: number;
-  valueOf: () => number;
-  index: number;
-}
 
 @Component
 export default class PieChart extends Vue {
-  private data: PieData[];
+  @Prop({
+    default: 450,
+    type: Number,
+  })
+  private height!: number;
+
+  @Prop({
+    default: 450,
+    type: Number,
+  })
+  private width!: number;
+
+  @Prop({
+    default: () => [],
+    type: Array,
+  })
+  private data!: PieData[];
+
+  @Prop({
+    default: 40,
+    type: Number,
+  })
+  private margin!: number;
+
   private radius: number;
-  private height: number;
-  private width: number;
-  private margin: number;
 
   constructor() {
     super();
-    this.data = [{
-      name: '<5',
-      value: 5,
-      valueOf: () => 5,
-      index: 0,
-    }, {
-      name: '<10',
-      value: 10,
-      valueOf: () => 10,
-      index: 1,
-    }, {
-      name: '<10xxx',
-      value: 20,
-      valueOf: () => 20,
-      index: 1,
-    }, {
-      name: '<xxxx',
-      value: 30,
-      valueOf: () => 30,
-      index: 1,
-    }];
-    this.height = 450;
-    this.width = 450;
-    this.margin = 40;
-    this.radius = Math.min(this.width, this.height) / 2 - this.margin
+    this.radius = 0;
   }
 
   mounted() {
+    this.radius = Math.min(this.width, this.height) / 2 - this.margin
     const svg = d3.create('svg')
       .attr('width', `${this.width}`)
       .attr('height', `${this.height}`)
       .attr("viewBox", `0 0 ${this.width} ${this.height}`);
 
-    const g = svg.append('g').attr('transform', `translate(${this.width / 2}, ${this.height / 2})`);
+    const g = svg.append('g')
+      .attr(
+        'transform',
+        `translate(${this.width / 2}, ${this.height / 2})`
+      );
 
     const color = d3.scaleOrdinal()
       .domain(this.data.map(data => data.name))
-      .range([
-        "#98abc5",
-        "#8a89a6",
-        "#7b6888",
-        "#6b486b",
-        "#a05d56"
-      ])
+      .range(d3.schemeDark2)
 
     const pie = d3.pie()
       .padAngle(0.5)
       .sort(null)
       .value(data => data.valueOf());
-    const arcs = pie(this.data);
+    const arcs = pie(this.data.map(data => data.value));
 
     const arc = d3.arc().innerRadius(100).outerRadius(this.radius);
     g.selectAll("whatever")
@@ -76,20 +65,38 @@ export default class PieChart extends Vue {
       .enter()
       .append('path')
         .attr('d', d => arc({
-          innerRadius: 100,
+          innerRadius: this.margin,
           outerRadius: this.radius,
           startAngle: d.startAngle,
           endAngle: d.endAngle,
         }))
         .attr('fill', d => {
-          console.log(d);
           const result =  color(`${d.value}`);
           return `${result}`;
         })
-        .attr("stroke", "black")
-        .style("stroke-width", "2px")
-        .style("opacity", 0.7)
+        .attr('stroke', 'white')
+        .attr('stroke-width', '2px')
+        .attr('opacity', 0.7);
 
+    g.selectAll('whatever')
+      .data(arcs)
+      .enter()
+      .append('text')
+      .text(d => {
+        const name = this.data[d.index].name;
+        return `${name}: ${d.value}`
+      })
+      .attr("transform", d => {
+        const result = arc.centroid({
+          innerRadius: this.margin,
+          outerRadius: this.radius,
+          startAngle: d.startAngle,
+          endAngle: d.endAngle,
+        });
+        return `translate(${result})`;
+      })
+      .style("text-anchor", "middle")
+      .style("font-size", "11px");
 
     this.$el.append(svg.node() as Node);
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,9 @@
-<template>
-  <div class="home">
-   <PieChart />
-  </div>
-</template>
-
-<script>
+<script lang="ts">
 import RegionMap from '@/components/RegionMap.vue';
 import PieChart from '@/components/PieChart.vue';
 import { Component, Vue } from 'vue-property-decorator';
+import todayData from '@/assets/data/covid-19-gr-2020-03-24.json';
+import PieData from '../types/PieData';
 
 @Component({
   name: 'Home',
@@ -17,5 +13,48 @@ import { Component, Vue } from 'vue-property-decorator';
   },
 })
 export default class Home extends Vue {
+  data: any; // eslint-disable-line
+
+  constructor() {
+    super();
+    this.data = todayData;
+  }
+
+  get totalCases(): number {
+    return todayData.total;
+  }
+
+  get casesGroupedByAge(): PieData[] {
+    return [{
+      name: '65+',
+      value: +todayData['65_plus'],
+    }, {
+      name: '40-64',
+      value: +todayData['40-64'],
+    }, {
+      name: '18-39',
+      value: +todayData['18-39'],
+    }, {
+      name: '0-17',
+      value: +todayData['0-17'],
+    }].reverse();
+  }
 }
 </script>
+
+<template>
+  <article class="home-view">
+    <section class="home-view__card">
+      <h2>Σύνολo</h2>
+       {{ totalCases }}
+    </section>
+   <PieChart :data="casesGroupedByAge" />
+  </article>
+</template>
+
+<style lang="sass">
+.home-view
+  &__card
+    border: 1px solid green
+    border-radius: 2px
+</style>
