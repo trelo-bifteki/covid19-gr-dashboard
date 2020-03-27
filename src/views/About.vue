1828b6cfb4828c9adfc69c7aa9691d0d7d8ed353
Lampros Papadimitriou <papadimitriou@tyntec.com>
2020-03-27 22:16:32 +0100

    Add Jenkinsfile

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>This is a simple dashboard for covid-19 staus in Greece</p>
+    <p>Stay safe!</p>
   </div>
 </template>
